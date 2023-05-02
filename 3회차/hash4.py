60675629548044d170a40fe3519f6a715cf33e8e
pjh9712 <ses88498@gmail.com>
2023-05-02 21:11:10 +0900

    upload

--- 3회차/hash4.py
+++ 3회차/hash4.py
@@ -5,13 +5,13 @@ def solution(clothes):
     answer = 1
     dic = {}
     
-    # clothes 리스트를 순회하며, 각 옷 종류(c_kind)에 해당하는 옷 이름(c_name)을 hash_dict 딕셔너리에 추가합니다.
+    # 각 옷 종류(c_kind)에 해당하는 옷 이름(c_name)을 딕셔너리에 추가합니다.
     for c_name, c_kind in clothes:
         if c_kind in dic:
             dic[c_kind].append(c_name)
         else:
             dic[c_kind] = [c_name]
-    
+
     # 딕셔너리의 모든 키(key)에 대해, (해당 옷 종류의 개수 + 1)을 answer에 곱해줍니다.
     for key in dic.keys():
         answer *= (len(dic[key]) + 1)
