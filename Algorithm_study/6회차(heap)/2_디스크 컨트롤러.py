800380df3e60c8e7e7872b8ad830444a38207c4e
pjh9712 <ses88498@gmail.com>
2023-06-28 18:51:03 +0900

    commit

--- Algorithm_study/6회차(heap)/2_디스크 컨트롤러.py
+++ Algorithm_study/6회차(heap)/2_디스크 컨트롤러.py
@@ -2,46 +2,21 @@ import heapq
 
 def solution(jobs):
     total = 0
-    job_idx = 0
     cur_time = 0
+    n = len(jobs)
     jobs.sort(key=lambda x: x[0]) # 요청 시간으로 정렬
     heap = []
 
-    while j < len(jobs) or heap:
-        if j < len(jobs) and cur_time >= jobs[j][0]:
-            heapq.heappush(heap, (jobs[j][1], jobs[j][0]))
-            j += 1
+    while jobs or heap:
+        while jobs and jobs[0][0] <= cur_time:
+            heapq.heappush(heap, jobs.pop(0)[::-1])
+
+        if not heap:
+            cur_time = jobs[0][0]
             continue
 
-        if heap:
-            job = heapq.heappop(heap)
-            cur_time += job[0]
-            total += cur_time - job[1]
-        else:
-            cur_time = jobs[j][0]
+        process = heapq.heappop(heap)
+        cur_time += process[0]
+        total += cur_time - process[1]
 
-    return total // len(jobs)
-
-import heapq
-
-def solution(jobs):
-    total = 0
-    
-    cur_time = 0
-    
-    jobs.sort(key=lambda x: x[0])
-
-    heap = []
-
-    while heap:
-        for request, processing in jobs:
-            if cur_time >= request:
-                heapq.heappush(heap, (processing, request))
-
-        if heap:
-            processing, request = heapq.heappop(heap)
-            cur_time += processing
-            total += cur_time - request
-        else:
-            cur_time = 
-            
+    return total // n
